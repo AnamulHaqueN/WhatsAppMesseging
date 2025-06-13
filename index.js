3c01ea3a43547ba498abee66e343f3f18c7feadf
AnamulHaqueN <rafaielanam@gmail.com>
2025-06-13 07:48:52 +0600

    connect mongoose and add path for ejs

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 8080;
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", ejs);
+
+main().then((res) => {
+    console.log("Connection is successful !");
+})
+.catch((err) => {
+    console.log(err);
+})
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
 
 app.listen(port, () => {
     console.log(`App is listening port ${port} !`);
+});
+
+app.get("/", (req, res) => {
+    res.send("app is responding");
 });
