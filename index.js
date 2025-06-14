37564c8468f5ea7f0de674b8f5ce63ae34498883
AnamulHaqueN <rafaielanam@gmail.com>
2025-06-14 09:09:44 +0600

    add chats route and create views to show chats in chats route

--- index.js
+++ index.js
@@ -19,24 +19,18 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
 
-let chat1 = new Chat({
-    from: "Neha",
-    to: "priya",
-    msg: "Hi i need your books",
-    created_at: new Date()
-});
-
-chat1.save().then((res) => {
-    console.log(res);
-})
-.catch((err) => {
-    console.log(err);
+// index route
+app.get("/chats", async(req, res) => {
+    let chats = await Chat.find();
+    console.log(chats);
+    res.render("index.ejs", {chats});
 })
 
 app.listen(port, () => {
     console.log(`App is listening port ${port} !`);
 });
 
+// rote route
 app.get("/", (req, res) => {
     res.send("app is responding");
 });
